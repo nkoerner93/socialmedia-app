558a2c7e8507f7ffa2f4d869df1853054a32e069
Nicolas Körner <nkoerner93@gmail.com>
2023-11-17 15:49:46 +0100

    Setup basic page routes, auth folder and sign-up/in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+import "./globals.css";
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+
+        {/* private routes */}
+
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
