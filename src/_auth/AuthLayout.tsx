5358f16e7d6ae741440f98761708a0d037bdc5ac
Nicolas Körner <nkoerner93@gmail.com>
2023-11-18 15:15:42 +0100

    Added ShadCN React-Hook Form with zod schema to signup-layout, added assets folder + files.

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,27 @@
+import { Navigate, Outlet } from "react-router-dom";
+
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden lg:block h-screen w-1/2 object-cover bg-norepeat"
+            rel="preload"
+          />
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
