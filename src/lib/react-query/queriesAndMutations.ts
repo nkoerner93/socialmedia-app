fba9d35475a23ce4da3225aaa3fc8c41e2882d10
Nicolas Körner <nkoerner93@gmail.com>
2024-01-15 09:33:51 +0100

    Add Authentication Context with types, auth context + provider to check authentication and getCurrentUser to get User from DB.

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -13,7 +13,7 @@ export const useCreateUserAccount = () => {
     })
 }
 
-export const useSignInAccountMutation = () => {
+export const useSignInAccount = () => {
     return useMutation({
         mutationFn: (user: {email: string; password:string}) => signInAccount(user)
     })
