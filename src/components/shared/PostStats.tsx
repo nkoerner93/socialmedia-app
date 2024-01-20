8e22d56bf4d042497914f82590c38951aa9d4f8d
Nicolas Körner <nkoerner93@gmail.com>
2024-01-20 23:34:32 +0100

    fix handle typo

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -13,7 +13,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
     setLiked(true);
   };
 
-  const handledisLikePost = () => {
+  const handleisLikePost = () => {
     setLiked(false);
   };
 
@@ -24,7 +24,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
           src="/assets/icons/liked.svg"
           alt="liked"
           className="w-6 h-6"
-          onClick={handledisLikePost}
+          onClick={handleisLikePost}
         />
       ) : (
         <img
